1740fa2b3ca2485331a63593555d7cda3e074602
hebidu <hebiduhebi@126.com>
2017-11-17 18:28:51 +0800

    single spider complete beta version

---
+++ application/index/config.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('ITBOYE_CDN')) {
+    define("ITBOYE_CDN", "http://bs.cdn.qqav.club");
+}
+
+return [
+    'site_url' => 'https://bs.qqav.club',
+    // 默认输出类型
+    'default_return_type' => 'html',
+    'view_replace_str' => [
+        '__PUBLIC__' => ROOT_PATH . '/static/' . request()->module() . '',
+        '__JS__' => ROOT_PATH . '/static/' . request()->module() . '/js',
+        '__CSS__' => ROOT_PATH . '/static/' . request()->module() . '/css',
+        '__IMG__' => ROOT_PATH . '/static/' . request()->module() . '/img',
+        '__SELF__' => request()->url(),
+        '__CDN__' => ITBOYE_CDN,
+        '__APP_VERSION__' => time()
+    ],
+    'session' => [
+        // SESSION_ID的提交变量,解决flash上传跨域
+        'var_session_id' => 'itboye_sid',
+        // SESSION 前缀
+        'prefix' => 'itboye_index',
+        // 驱动方式 支持redis memcache memcached
+        'type' => '',
+        // 是否自动开启 SESSION
+        'auto_start' => true
+    ],
+];
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,25 +3,17 @@
 namespace app\index\controller;
 
 
-use by\component\bookstore\v1\entity\BookEntity;
-use by\infrastructure\helper\Object2DataArrayHelper;
+use app\component\spider\xia_shu\repo\XiaShuBookRepo;
 use think\Controller;
 
 class Index extends Controller
 {
-    public function arch()
-    {
-        $entity = new BookEntity();
-        $entity->setAuthorId(1);
-        $entity->setAuthorName('hebidu');
-        $arr = Object2DataArrayHelper::getDataArrayFrom($entity);
-
-        return $this->display(json_encode($arr));
-    }
 
     public function index()
     {
+        $books = (new XiaShuBookRepo())->limit(0, 100)->select();
 
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+        $this->assign('books', $books);
+        return $this->fetch();
     }
 }
