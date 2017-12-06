6552d437500be8866bffc667cf2ada8d4151fd16
hebidu <hebiduhebi@126.com>
2017-12-06 17:45:29 +0800

    【增加】完成接口基础的迁移

---
+++ application/domain/BookDomain.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * 注意：本内容仅限于博也公司内部传阅,禁止外泄以及用于其他的商业目的
+ * @author    hebidu<346551990@qq.com>
+ * @copyright 2017 www.itboye.com Boye Inc. All rights reserved.
+ * @link      http://www.itboye.com/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * Revision History Version
+ ********1.0.0********************
+ * file created @ 2017-12-06 17:03
+ *********************************
+ ********1.0.1********************
+ *
+ *********************************
+ */
+
+namespace app\domain;
+
+
+use by\domain\BaseDomain;
+
+/**
+ * Class BookDomain
+ * 小说信息接口
+ * @package app\domain
+ */
+class BookDomain extends BaseDomain
+{
+    /**
+     * 搜索
+     */
+    public function search()
+    {
+
+    }
+
+    /**
+     * 书籍详情
+     */
+    public function detail()
+    {
+//        $id = $this->_post()
+    }
+
+    /**
+     * 书籍来源信息
+     */
+    public function source_list()
+    {
+
+    }
+
+    /**
+     * 书籍每页信息
+     */
+    public function pages()
+    {
+
+    }
+
+    /**
+     * 书页详情内容
+     */
+    public function page_content()
+    {
+
+    }
+}
