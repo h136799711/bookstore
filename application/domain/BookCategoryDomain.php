67938e588c74a18f47850a287be5f475c42e4378
hebidu <hebiduhebi@126.com>
2017-12-09 17:57:21 +0800

    【增加】初步完成

---
+++ application/domain/BookCategoryDomain.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 注意：本内容仅限于博也公司内部传阅,禁止外泄以及用于其他的商业目的
+ * @author    hebidu<346551990@qq.com>
+ * @copyright 2017 www.itboye.com Boye Inc. All rights reserved.
+ * @link      http://www.itboye.com/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * Revision History Version
+ ********1.0.0********************
+ * file created @ 2017-12-09 17:55
+ *********************************
+ ********1.0.1********************
+ *
+ *********************************
+ */
+
+namespace app\domain;
+
+
+use by\domain\BaseDomain;
+
+/**
+ * Class BookCategoryDomain
+ * 书籍分类
+ * @package app\domain
+ */
+class BookCategoryDomain extends BaseDomain
+{
+
+    /**
+     * 分类查询
+     *
+     */
+    public function query()
+    {
+
+    }
+
+}
